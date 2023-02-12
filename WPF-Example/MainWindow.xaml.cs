68b2026f8eb327cb0ad14a2a5535739707d59b3f
Mike <Mike@DESKTOP-58BK2MJ>
2023-02-12 14:57:32 +0100

    Add project files.

---
+++ WPF-Example/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ControlzEx.Theming;
+using MahApps.Metro.Theming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Example
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //ThemeManager.Current.ChangeTheme(this, "Dark.Green");
+           
+            var theme = ThemeManager.Current.AddLibraryTheme(
+            new LibraryTheme(
+                new Uri("pack://application:,,,/WPF-Example;component/CustomDarkAccent.xaml"),
+                MahAppsLibraryThemeProvider.DefaultInstance
+                )
+            );
+
+            ThemeManager.Current.ChangeTheme(this, theme);
+        }
+    }
+}
